509047d8b97249189a317efb8999d9fcc0a256ed
Sergio <sergioobolevich@gmail.com>
2020-12-15 15:01:39 -0300

    Add category_id to article. Add basic form for article creation.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,28 @@
 class ArticlesController < ApplicationController
+  def index
+  end
+  
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = current_user.articles.build
+  end
+
+  def create
+    article = current_user.articles.build(article_params)
+    if article.save
+      flash[:notice] = 'The article was created succesfully.'
+    else
+      flash[:notice] = 'Some error ocurred. Try again.'
+    end
+    redirect_to article_path(article)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'articles#show'
+  root to: 'articles#index'
   devise_for :users
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
