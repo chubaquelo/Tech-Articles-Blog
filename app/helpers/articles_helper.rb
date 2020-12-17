3716c983ba0e0982f751d66363278c0de4cc4366
Sergio <sergioobolevich@gmail.com>
2020-12-17 00:59:21 -0300

    Add footer layout.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module ArticlesHelper
   def last_articles_per_category
     @last_articles.length.times do |pos|
       concat content_tag(:div, link_to(@last_articles[pos].category.name, category_path(@last_articles[pos].category),
-                        class: 'h5 text-white d-inline-block h5-underline font-weight-bold') +
+                        class: 'h5 text-white d-inline-block h5-underline font-weight-bold text-decoration-none') +
                         content_tag(:h4, link_to(@last_articles[pos].title, article_path(@last_articles[pos]), class: 'text-white text-decoration-none'), class: 'text-white article-title-h5 font-weight-bold secondary-articles-titles'),
                          class: 'col-6 col-lg-3 p-3', style: "background-image: url(#{url_for(@last_articles[pos].image)}); background-size: cover;")
       break if pos == @last_articles.length - 1
