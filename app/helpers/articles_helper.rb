ebd59cda4811941502943c73b4d1b7d6eebc716a
Sergio <sergioobolevich@gmail.com>
2020-12-17 00:47:48 -0300

    Improve articles index design.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,8 +4,8 @@ module ArticlesHelper
     @last_articles.length.times do |pos|
       concat content_tag(:div, link_to(@last_articles[pos].category.name, category_path(@last_articles[pos].category),
                         class: 'h5 text-white d-inline-block h5-underline font-weight-bold') +
-                        content_tag(:h4, link_to(@last_articles[pos].title, article_path(@last_articles[pos]), class: 'text-white text-decoration-none'), class: 'text-white article-title-h5 font-weight-bold'),
-                         class: 'col-6 col-lg-3 p-3', style: "background-image: url(#{url_for(@last_articles[pos].image)}); background-position: center;")
+                        content_tag(:h4, link_to(@last_articles[pos].title, article_path(@last_articles[pos]), class: 'text-white text-decoration-none'), class: 'text-white article-title-h5 font-weight-bold secondary-articles-titles'),
+                         class: 'col-6 col-lg-3 p-3', style: "background-image: url(#{url_for(@last_articles[pos].image)}); background-size: cover;")
       break if pos == @last_articles.length - 1
 
     end
